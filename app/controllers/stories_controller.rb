6bd6e9a4b62c88ce3ce7f44b8cb0e13dffd34887
Kerron S. Streater <kerronstreater@gmail.com>
2018-10-30 23:40:20 -0400

    kept working on stories, yup 10/29/18

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -13,9 +13,36 @@ class StoriesController < ApplicationController
   def edit
   end
 
+  def create
+    @story = Story.new(story_params)
+
+    respond_to do |format|
+      if @story.save
+        format.html { redirect_to root_path }
+#        format.json { render :show, status: :created, location: @story }
+      else
+        format.html { render :new }
+#        format.json { render json: @story.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
   end
 
   def destroy
   end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def set_story
+      @story = Story.find(params[:id])
+    end
+
+    def story_params
+      params.require(:story).permit(:author, :comment, :title, :puslished, :story, :image)
+    end
 end
